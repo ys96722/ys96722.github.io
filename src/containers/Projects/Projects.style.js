1fdcdef56fdd6c5782d199b76d897821998cda42
Yoon Chang <yoonsees@gmail.com>
2019-02-22 02:28:36 -0600

    Update Projects.style.js

--- src/containers/Projects/Projects.style.js
+++ src/containers/Projects/Projects.style.js
@@ -9,26 +9,6 @@ import {
   subtitle_header
 } from "../../style/variables";
 
-export const Index = styled.div`
-  position: absolute;
-  left: 0;
-  bottom: 0;
-  z-index: -1;
-  h1 {
-    font-size: ${section_header};
-    font-weight: bold;
-    color: ${yellow};
-    opacity: 0.6;
-    transform: translateY(20%);
-  }
-`;
-
-export const ImageContainer = styled.div`
-  width: 800px;
-  margin-left: auto;
-  margin-right: auto;
-`;
-
 export const Button = styled.button`
   background-color: ${black};
   border: none;
@@ -51,3 +31,24 @@ export const Button = styled.button`
       color: white;
     `}
 `;
+
+/* 
+export const Index = styled.div`
+  position: absolute;
+  left: 0;
+  bottom: 0;
+  z-index: -1;
+  h1 {
+    font-size: ${section_header};
+    font-weight: bold;
+    color: ${yellow};
+    opacity: 0.6;
+    transform: translateY(20%);
+  }
+`;
+
+export const ImageContainer = styled.div`
+  width: 800px;
+  margin-left: auto;
+  margin-right: auto;
+`; */
