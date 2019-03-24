3e493e4bf6e912e69c10deea10fe84ce3bc6eddf
Yoon Chang <yoonsees@gmail.com>
2019-03-24 12:34:44 -0500

    Replace 'ItemMastery' Component
    Replacing 'ItemMastery' component style in order to improve readability

--- src/containers/Resume/ItemMastery.js
+++ src/containers/Resume/ItemMastery.js
@@ -1,59 +1,99 @@
-import React from "react";
-import { A, OpenButton } from "../../style/types";
+import React, { Component } from "react";
+import { A, OpenButton, P } from "../../style/types";
 import { Div, Flex } from "../../style/grid";
-import { red, blue, green } from "../../style/variables";
+import { red, blue, green, black } from "../../style/variables";
 
 // React component for the card (main component)
-const itemMastery = props => {
+export default class ItemMastery extends Component {
   // Props: (wp or react), img, title, header, content, link
-  const boxes = [];
+  state = {
+    boxes: []
+  };
 
-  // Generate Project boxes
-  if (props.projects) {
-    for (let i = 0; i < props.projects; i++) {
-      boxes.push(
-        <div
-          style={{
-            width: "8px",
-            height: "100%",
-            backgroundColor: "#f25f5c",
-            // backgroundColor: "#60b6db",
-            marginLeft: "0.1rem",
-            borderRadius: "10px"
-          }}
-        />
-      );
+  renderCircle = () => {
+    for (let i = 0; i < 5; i++) {
+      if (i <= this.props.mastery) {
+        this.setState(prevState => ({
+          boxes: [
+            ...prevState.boxes,
+            <div
+              style={{
+                width: "20px",
+                height: "20px",
+                border: "1px solid #424242",
+                backgroundColor: "#424242",
+                marginLeft: "0.1rem",
+                borderRadius: "50%"
+              }}
+            />
+          ]
+        }));
+      } else {
+        this.setState(prevState => ({
+          boxes: [
+            ...prevState.boxes,
+            <div
+              style={{
+                width: "20px",
+                height: "20px",
+                border: "1px solid #424242",
+                backgroundColor: "white",
+                // backgroundColor: "#60b6db",
+                marginLeft: "0.1rem",
+                borderRadius: "50%"
+              }}
+            />
+          ]
+        }));
+      }
     }
-  }
+  };
 
-  if (props.semesters) {
-    for (let i = 0; i < props.semesters; i++) {
-      boxes.push(
-        <div
-          style={{
-            width: "8px",
-            height: "100%",
-            backgroundColor: "#5baaa5",
-            marginLeft: "0.1rem",
-            borderRadius: "10px"
-          }}
-        />
-      );
-    }
-  }
+  // boxes[i] = (
+  //   <div
+  //     style={{
+  //       width: "20px",
+  //       height: "20px",
+  //       backgroundColor: "#424242",
+  //       // backgroundColor: "#60b6db",
+  //       marginLeft: "0.1rem",
+  //       borderRadius: "50%"
+  //     }}
+  //   />
+  // );
+
+  // if (props.semesters) {
+  //   for (let i = 0; i < props.semesters; i++) {
+  //     boxes.push(
+  //       <div
+  //         style={{
+  //           width: "8px",
+  //           height: "100%",
+  //           backgroundColor: "#5baaa5",
+  //           marginLeft: "0.1rem",
+  //           borderRadius: "10px"
+  //         }}
+  //       />
+  //     );
+  //   }
+  // }
 
   // Generate Semester boxes
 
-  return (
-    <Flex
-      direction="row"
-      style={{
-        justifyContent: "flex-end"
-      }}
-    >
-      {boxes}
-    </Flex>
-  );
-};
+  componentWillMount = () => {
+    this.renderCircle();
+  };
 
-export default itemMastery;
+  render() {
+    return (
+      <Flex
+        direction="row"
+        style={{
+          justifyContent: "flex-end"
+        }}
+      >
+        {this.state.boxes}
+      </Flex>
+    );
+  }
+}
--- src/containers/Resume/Resume.js
+++ src/containers/Resume/Resume.js
@@ -36,7 +36,7 @@ export default class Resume extends Component {
         <ResumeContainer animation="fadeIn">
           <SectionContainer>
             <Title>Skills</Title>
-            <InfoIndex />
+            {/* <InfoIndex /> */}
             <Section flexDirection="row" justifyContent="space-evenly">
               <Column bgColor={red}>
                 <ColumnTitle bgColor={red}>Data</ColumnTitle>
@@ -45,55 +45,58 @@ export default class Resume extends Component {
                   <h2>Languages</h2>
                   <Item>
                     <ItemName>Python</ItemName>
-                    <ItemMastery semesters={6} />
-                  </Item>
-
-                  <Item>
-                    <ItemName>R</ItemName>
-                    <ItemMastery semesters={6} />
-                  </Item>
-
-                  <Item>
-                    <ItemName>SAS</ItemName>
-                    <ItemMastery semesters={2} />
-                  </Item>
-
-                  <Item>
-                    <ItemName>Pig</ItemName>
-                    <ItemMastery semesters={1} />
+                    <ItemMastery mastery={4} />
                   </Item>
 
                   <Item>
                     <ItemName>SQL</ItemName>
-                    <ItemMastery projects={4} semesters={3} />
+                    <ItemMastery mastery={4} />
+                  </Item>
+
+                  <Item>
+                    <ItemName>R</ItemName>
+                    <ItemMastery mastery={4} />
+                  </Item>
+
+                  <Item>
+                    <ItemName>SAS</ItemName>
+                    <ItemMastery mastery={2} />
+                  </Item>
+
+                  <Item>
+                    <ItemName>Pig</ItemName>
+                    <ItemMastery mastery={1} />
                   </Item>
                 </SubColumn>
                 <br />
                 <SubColumn>
                   <h2>Database</h2>
-                  <Item>
-                    <ItemName>Firebase</ItemName>
-                    <ItemMastery projects={2} semesters={1} />
-                  </Item>
+
                   <Item>
                     <ItemName>SQLite3</ItemName>
-                    <ItemMastery projects={5} semesters={2} />
+                    <ItemMastery mastery={4} />
                   </Item>
+
                   <Item>
                     <ItemName>MySQL</ItemName>
-                    <ItemMastery projects={2} semesters={2} />
+                    <ItemMastery mastery={3} />
                   </Item>
                   <Item>
                     <ItemName>PostgreSQL</ItemName>
-                    <ItemMastery projects={2} semesters={1} />
+                    <ItemMastery mastery={3} />
                   </Item>
                   <Item>
+                    <ItemName>Firebase</ItemName>
+                    <ItemMastery mastery={2} />
+                  </Item>
+
+                  <Item>
                     <ItemName>RSQLite</ItemName>
-                    <ItemMastery semesters={2} />
+                    <ItemMastery mastery={2} />
                   </Item>
                   <Item>
                     <ItemName>Hive</ItemName>
-                    <ItemMastery semesters={1} />
+                    <ItemMastery mastery={1} />
                   </Item>
                 </SubColumn>
                 <br />
@@ -101,24 +104,24 @@ export default class Resume extends Component {
                   <h2>Tools</h2>
                   <Item>
                     <ItemName>Anaconda3</ItemName>
-                    <ItemMastery semesters={1} projects={6} />
-                  </Item>
-                  <Item>
-                    <ItemName>Hadoop</ItemName>
-                    <ItemMastery semesters={1} />
-                  </Item>
-                  <Item>
-                    <ItemName>Scikit-Learn</ItemName>
-                    <ItemMastery semesters={2} projects={2} />
+                    <ItemMastery mastery={4} />
                   </Item>
                   <Item>
                     <ItemName>Pandas & Numpy</ItemName>
-                    <ItemMastery semesters={2} projects={5} />
+                    <ItemMastery mastery={4} />
+                  </Item>
+                  <Item>
+                    <ItemName>Hadoop</ItemName>
+                    <ItemMastery mastery={3} />
+                  </Item>
+                  <Item>
+                    <ItemName>Scikit-Learn</ItemName>
+                    <ItemMastery mastery={3} />
                   </Item>
 
                   <Item>
                     <ItemName>Seaborn & PyPlot</ItemName>
-                    <ItemMastery semesters={2} projects={5} />
+                    <ItemMastery mastery={3} />
                   </Item>
                 </SubColumn>
               </Column>
@@ -129,23 +132,23 @@ export default class Resume extends Component {
                   <h2>Languages</h2>
                   <Item>
                     <ItemName>JavaScript</ItemName>
-                    <ItemMastery semesters={3} projects={8} />
+                    <ItemMastery mastery={5} />
                   </Item>
                   <Item>
                     <ItemName>Ruby</ItemName>
-                    <ItemMastery projects={2} />
+                    <ItemMastery mastery={2} />
                   </Item>
                   <Item>
                     <ItemName>PHP</ItemName>
-                    <ItemMastery semesters={1} projects={7} />
+                    <ItemMastery mastery={3} />
                   </Item>
                   <Item>
                     <ItemName>C#</ItemName>
-                    <ItemMastery semesters={1} projects={2} />
+                    <ItemMastery mastery={2} />
                   </Item>
                   <Item>
                     <ItemName>Java</ItemName>
-                    <ItemMastery semesters={2} />
+                    <ItemMastery mastery={2} />
                   </Item>
                 </SubColumn>
                 <br />
@@ -153,23 +156,23 @@ export default class Resume extends Component {
                   <h2>Frameworks</h2>
                   <Item>
                     <ItemName>React.js</ItemName>
-                    <ItemMastery semesters={4} projects={3} />
-                  </Item>
-                  <Item>
-                    <ItemName>Ruby on Rails</ItemName>
-                    <ItemMastery semesters={1} projects={2} />
+                    <ItemMastery mastery={3} />
                   </Item>
                   <Item>
                     <ItemName>WordPress</ItemName>
-                    <ItemMastery semesters={2} projects={7} />
+                    <ItemMastery mastery={4} />
+                  </Item>
+                  <Item>
+                    <ItemName>Ruby on Rails</ItemName>
+                    <ItemMastery mastery={2} />
                   </Item>
                   <Item>
                     <ItemName>Unity 3D</ItemName>
-                    <ItemMastery semesters={1} projects={2} />
+                    <ItemMastery mastery={2} />
                   </Item>
                   <Item>
                     <ItemName>Android Studio</ItemName>
-                    <ItemMastery semesters={1} />
+                    <ItemMastery mastery={1} />
                   </Item>
                 </SubColumn>
                 <br />
@@ -177,23 +180,23 @@ export default class Resume extends Component {
                   <h2>Tools</h2>
                   <Item>
                     <ItemName>BootStrap</ItemName>
-                    <ItemMastery projects={7} />
+                    <ItemMastery mastery={4} />
                   </Item>
                   <Item>
                     <ItemName>Webpack</ItemName>
-                    <ItemMastery projects={2} />
+                    <ItemMastery mastery={2} />
                   </Item>
                   <Item>
                     <ItemName>Redux</ItemName>
-                    <ItemMastery projects={2} />
+                    <ItemMastery mastery={2} />
                   </Item>
                   <Item>
                     <ItemName>Axios</ItemName>
-                    <ItemMastery projects={2} />
+                    <ItemMastery mastery={2} />
                   </Item>
                   <Item>
                     <ItemName>Jest</ItemName>
-                    <ItemMastery projects={2} />
+                    <ItemMastery mastery={2} />
                   </Item>
                 </SubColumn>
               </Column>
@@ -204,20 +207,20 @@ export default class Resume extends Component {
                   <h2>Languages</h2>
                   <Item>
                     <ItemName>Bash</ItemName>
-                    <ItemMastery semesters={6} projects={6} />
+                    <ItemMastery mastery={4} />
                   </Item>
                   <Item>
                     <ItemName>C</ItemName>
-                    <ItemMastery semesters={1} />
+                    <ItemMastery mastery={2} />
                   </Item>
                   <Item>
                     <ItemName>C++</ItemName>
-                    <ItemMastery semesters={1} />
+                    <ItemMastery mastery={2} />
                   </Item>
 
                   <Item>
                     <ItemName>Verilog</ItemName>
-                    <ItemMastery semesters={1} />
+                    <ItemMastery mastery={1} />
                   </Item>
                 </SubColumn>
                 <br />
@@ -225,23 +228,23 @@ export default class Resume extends Component {
                   <h2>Architectures</h2>
                   <Item>
                     <ItemName>OOP</ItemName>
-                    <ItemMastery semesters={4} projects={5} />
+                    <ItemMastery mastery={4} />
                   </Item>
                   <Item>
                     <ItemName>CBD</ItemName>
-                    <ItemMastery semesters={1} />
+                    <ItemMastery mastery={3} />
                   </Item>
                   <Item>
                     <ItemName>MVC</ItemName>
-                    <ItemMastery semesters={1} projects={2} />
+                    <ItemMastery mastery={2} />
                   </Item>
                   <Item>
                     <ItemName>REST API</ItemName>
-                    <ItemMastery semesters={1} />
+                    <ItemMastery mastery={2} />
                   </Item>
                   <Item>
                     <ItemName>TDD</ItemName>
-                    <ItemMastery semesters={1} />
+                    <ItemMastery mastery={1} />
                   </Item>
                 </SubColumn>
                 <br />
@@ -249,11 +252,11 @@ export default class Resume extends Component {
                   <h2>Tools</h2>
                   <Item>
                     <ItemName>Git</ItemName>
-                    <ItemMastery semesters={6} projects={6} />
+                    <ItemMastery mastery={4} />
                   </Item>
                   <Item>
                     <ItemName>VSCode</ItemName>
-                    <ItemMastery semesters={6} projects={6} />
+                    <ItemMastery mastery={3} />
                   </Item>
                 </SubColumn>
               </Column>
@@ -262,7 +265,7 @@ export default class Resume extends Component {
           <br />
           <br />
           <SectionContainer>
-              {/* <Row>
+            {/* <Row>
                 <Column width="30%" textAlign="left">
                   <CompanyTitle>University of Illinois</CompanyTitle>
                   <CompanyInfo>
@@ -291,34 +294,31 @@ export default class Resume extends Component {
                   </ul>
                 </Column>
               </Row> */}
-              <Title>Academic</Title>
-              <Section flexDirection="column">
-                <Row>
-                  <Column width="50%" textAlign="left">
-                    <SchoolTitle>
-                      {/* <A href="https://kojobsillinois.com/" target="_blank"> */}
-                      University of Illinois at Urbana-Champaign
+            <Title>Academic</Title>
+            <Section flexDirection="column">
+              <Row>
+                <Column width="50%" textAlign="left">
+                  <SchoolTitle>
+                    {/* <A href="https://kojobsillinois.com/" target="_blank"> */}
+                    University of Illinois at Urbana-Champaign
                     {/* </A> */}
-                    </SchoolTitle>
-                    <SchoolInfo>Graduatation: May 2019</SchoolInfo>
-                  </Column>
-                  <Column width="50%" textAlign="right">
-                    <SchoolTitle fontWeight="400">
-                      Bachelors of Science in Statistics
                   </SchoolTitle>
-                    <SchoolInfo>Minor in Computer Science</SchoolInfo>
-                  </Column>
-                </Row>
-              </Section>
+                  <SchoolInfo>Graduatation: May 2019</SchoolInfo>
+                </Column>
+                <Column width="50%" textAlign="right">
+                  <SchoolTitle fontWeight="400">
+                    Bachelors of Science in Statistics
+                  </SchoolTitle>
+                  <SchoolInfo>Minor in Computer Science</SchoolInfo>
+                </Column>
+              </Row>
+            </Section>
             <Title>Experience</Title>
             <Section flexDirection="column" justifyContent="top">
               <Row>
                 <Column width="30%" textAlign="left">
                   <CompanyTitle>
-                    <A
-                      href="http://researchpark.illinois.edu/"
-                      target="_blank"
-                    >
+                    <A href="http://researchpark.illinois.edu/" target="_blank">
                       Enterprise Works
                     </A>
                   </CompanyTitle>
@@ -329,24 +329,22 @@ export default class Resume extends Component {
                   <JobTitle>Web Developer (Internship)</JobTitle>
                   <ul>
                     <Li>
-                      Develop eight websites for startups using WordPress,
-                      PHP, JavaScript, and CSS and produce tutorial videos
-                      for clients
+                      Develop eight websites for startups using WordPress, PHP,
+                      JavaScript, and CSS and produce tutorial videos for
+                      clients
                     </Li>
                     <Li>
-                      Coordinate two web development workshops to create a
-                      more cohesive workflow between designers and
-                      developers
+                      Coordinate two web development workshops to create a more
+                      cohesive workflow between designers and developers
                     </Li>
                     <Li>
-                      Reduce hours spent per website by approximately 10
-                      hours by devising a new SOP for web developers and
-                      graphic designers
+                      Reduce hours spent per website by approximately 10 hours
+                      by devising a new SOP for web developers and graphic
+                      designers
                     </Li>
                     <Li>
-                      Lead biweekly web development study group on
-                      JavaScript, CSS, and HTML5 to provide a basic
-                      development knowledge
+                      Lead biweekly web development study group on JavaScript,
+                      CSS, and HTML5 to provide a basic development knowledge
                     </Li>
                   </ul>
                 </Column>
@@ -362,23 +360,20 @@ export default class Resume extends Component {
                   <CompanyInfo>Champaign, IL</CompanyInfo>
                 </Column>
                 <Column width="70%" textAlign="left">
-                  <JobTitle>
-                    Founding President (Student Organization)
-                  </JobTitle>
+                  <JobTitle>Founding President (Student Organization)</JobTitle>
                   <ul>
                     <Li>
                       Create operating systems and provide administrative
                       support for HR, PR, Finance, and Curriculum department
                     </Li>
                     <Li>
-                      Design a Ruby on Rails curriculum with professional
-                      full stack developers to provide an approachable
-                      program for members
+                      Design a Ruby on Rails curriculum with professional full
+                      stack developers to provide an approachable program for
+                      members
                     </Li>
                     <Li>
                       Educate members by leading weekly Ruby on Rails study
-                      groups on MVC development, RESTful API, and HTTP
-                      methods.
+                      groups on MVC development, RESTful API, and HTTP methods.
                     </Li>
                   </ul>
                 </Column>
@@ -399,19 +394,18 @@ export default class Resume extends Component {
                   <JobTitle>Vice President (Student Organization)</JobTitle>
                   <ul>
                     <Li>
-                      Facilitate workshops with start-up entrepreneurs,
-                      content creators, and web developers to offer insights
-                      into start-up industry
+                      Facilitate workshops with start-up entrepreneurs, content
+                      creators, and web developers to offer insights into
+                      start-up industry
                     </Li>
                     <Li>
-                      Arrange mock interviews, resume sessions, and office
-                      tours by contacting professionals at local
-                      corporations and start-ups
+                      Arrange mock interviews, resume sessions, and office tours
+                      by contacting professionals at local corporations and
+                      start-ups
                     </Li>
                     <Li>
-                      Educate members by leading weekly Python study groups
-                      on OOP, data wrangling, visualization, and machine
-                      learning
+                      Educate members by leading weekly Python study groups on
+                      OOP, data wrangling, visualization, and machine learning
                     </Li>
                   </ul>
                 </Column>
