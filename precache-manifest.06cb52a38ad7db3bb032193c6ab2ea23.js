16047b954accb18afe3250fb846bbc577b0be2cf
Yoon Chang <yoonsees@gmail.com>
2019-04-16 11:32:45 -0500

    Updates

---
+++ precache-manifest.06cb52a38ad7db3bb032193c6ab2ea23.js
@@ -0,0 +1,238 @@
+self.__precacheManifest = [
+  {
+    "revision": "499e818969e4a43d3a69f9728a8b21f5",
+    "url": "/static/media/kojobs4.499e8189.png"
+  },
+  {
+    "revision": "71e7005967146458fba4",
+    "url": "/static/css/main.5f70d951.chunk.css"
+  },
+  {
+    "revision": "d983616a7e374a8ca234",
+    "url": "/static/js/1.d983616a.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "9a4fe352e064d49fb50c79965c8425ef",
+    "url": "/static/media/finalHero.9a4fe352.jpeg"
+  },
+  {
+    "revision": "0ef67f19c23efc5eb17d07ecdc9ed34a",
+    "url": "/static/media/tanks.0ef67f19.png"
+  },
+  {
+    "revision": "3c66201d9413eb69e764f046c0cc155e",
+    "url": "/static/media/space1.3c66201d.png"
+  },
+  {
+    "revision": "a0e49b1390bfa9b3354ff2b4c3fd27bd",
+    "url": "/static/media/space2.a0e49b13.png"
+  },
+  {
+    "revision": "90882e2ad49062eee7e859b84e9e83ad",
+    "url": "/static/media/space3.90882e2a.png"
+  },
+  {
+    "revision": "0170e2ebc62897cdccb64590949ad64d",
+    "url": "/static/media/space4.0170e2eb.png"
+  },
+  {
+    "revision": "f92885e52432d28c487299da4e11585f",
+    "url": "/static/media/hero.f92885e5.JPG"
+  },
+  {
+    "revision": "afd53a36809c8a4c660490a950f60cc5",
+    "url": "/static/media/diceroll.afd53a36.mp3"
+  },
+  {
+    "revision": "2c9d09a5791f7e79a3f549353df0de01",
+    "url": "/static/media/yooniverse1.2c9d09a5.png"
+  },
+  {
+    "revision": "0b2f25e5151a45b72f0c4195f88b7fc8",
+    "url": "/static/media/yooniverse2.0b2f25e5.png"
+  },
+  {
+    "revision": "0d08f0d8be948003549e10559b10907d",
+    "url": "/static/media/yooniverse3.0d08f0d8.png"
+  },
+  {
+    "revision": "ae39ad14285cde9e00ed8c89f7a8e338",
+    "url": "/static/media/yooniverse4.ae39ad14.png"
+  },
+  {
+    "revision": "b4159cab1b13f7feb87275bb7bac5783",
+    "url": "/static/media/simbiosys_hero.b4159cab.png"
+  },
+  {
+    "revision": "eaffee891c66bd44bbd7e16bac93f128",
+    "url": "/static/media/simbiosys1.eaffee89.png"
+  },
+  {
+    "revision": "43b6b2f4448334643552aac068c23ed2",
+    "url": "/static/media/simbiosys2.43b6b2f4.png"
+  },
+  {
+    "revision": "da2ed605335c58b91261b2b90857a4fe",
+    "url": "/static/media/simbiosys3.da2ed605.png"
+  },
+  {
+    "revision": "aa329cd927525ab96b467a9a13c8a108",
+    "url": "/static/media/native_hero.aa329cd9.png"
+  },
+  {
+    "revision": "16e30ba93073060977795580d6a86b7e",
+    "url": "/static/media/native1.16e30ba9.png"
+  },
+  {
+    "revision": "aceca249c439ae5e0b84fe435e1dbc4f",
+    "url": "/static/media/native2.aceca249.png"
+  },
+  {
+    "revision": "bee02acc53195abfbae323262273e468",
+    "url": "/static/media/native3.bee02acc.png"
+  },
+  {
+    "revision": "f1358816c2dba4d6a938d881e8c34b35",
+    "url": "/static/media/native4.f1358816.png"
+  },
+  {
+    "revision": "418eefdd447f764f794f031c53086578",
+    "url": "/static/media/kojobs_hero2.418eefdd.png"
+  },
+  {
+    "revision": "c43ed88287dcf9e712bc8b6607a660c8",
+    "url": "/static/media/kojobs1.c43ed882.png"
+  },
+  {
+    "revision": "322b6d7ae1bc0a1d1b6e5305c7d73521",
+    "url": "/static/media/kojobs2.322b6d7a.png"
+  },
+  {
+    "revision": "3cb87b24791a50c8e1ef5407fab2ae05",
+    "url": "/static/media/kojobs3.3cb87b24.png"
+  },
+  {
+    "revision": "71e7005967146458fba4",
+    "url": "/static/js/main.71e70059.chunk.js"
+  },
+  {
+    "revision": "f0d2e94f66e1d0c495fab9acf00f3542",
+    "url": "/static/media/kojobs5.f0d2e94f.png"
+  },
+  {
+    "revision": "7dc461b3532a13a86c8e6b55728314f5",
+    "url": "/static/media/ability_logo.7dc461b3.png"
+  },
+  {
+    "revision": "13e429a689cffc59922d27a6e28e2deb",
+    "url": "/static/media/ability1.13e429a6.png"
+  },
+  {
+    "revision": "48306035ff2c624d81cc98a79ff9d241",
+    "url": "/static/media/ability2.48306035.png"
+  },
+  {
+    "revision": "7258ddfbb5a79db58a169c38690c4d56",
+    "url": "/static/media/ability3.7258ddfb.png"
+  },
+  {
+    "revision": "db8ace3970c6cb9fc10ab8e940cc8a73",
+    "url": "/static/media/ability4.db8ace39.png"
+  },
+  {
+    "revision": "bc0c7cad6e300f0783540755b88cda99",
+    "url": "/static/media/ability5.bc0c7cad.png"
+  },
+  {
+    "revision": "23b6b207196f02b5bb170ba639a29541",
+    "url": "/static/media/2xl_blue_logo.23b6b207.png"
+  },
+  {
+    "revision": "1d39a8f56ff2b8e25e3c21161a1ee3f2",
+    "url": "/static/media/2xl1.1d39a8f5.png"
+  },
+  {
+    "revision": "2ddd583943b3f7c1cca6b31ee4b10cc7",
+    "url": "/static/media/2xl2.2ddd5839.png"
+  },
+  {
+    "revision": "45037f5e35cd412072aa6a14dbf8784a",
+    "url": "/static/media/2xl3.45037f5e.png"
+  },
+  {
+    "revision": "db81481c374651b3b964bce602808a99",
+    "url": "/static/media/2xl4.db81481c.png"
+  },
+  {
+    "revision": "def610b4a5b89b9edbea04d169215563",
+    "url": "/static/media/traverse_logo.def610b4.png"
+  },
+  {
+    "revision": "16a283053a2c4bdd985003f985c6fdd0",
+    "url": "/static/media/traverse1.16a28305.png"
+  },
+  {
+    "revision": "fd4efe300e0b53f1adcfeddde650c149",
+    "url": "/static/media/traverse2.fd4efe30.png"
+  },
+  {
+    "revision": "5fc906d48a94df4efc71f867a692f209",
+    "url": "/static/media/traverse3.5fc906d4.png"
+  },
+  {
+    "revision": "442194a602f714ae6f90def4b9010716",
+    "url": "/static/media/traverse4.442194a6.png"
+  },
+  {
+    "revision": "aa12c293ef53ede7d84cd3dfec5b975e",
+    "url": "/static/media/traverse5.aa12c293.png"
+  },
+  {
+    "revision": "0a4e89704631d75cbf43ae8d1f2fa678",
+    "url": "/static/media/tanks1.0a4e8970.png"
+  },
+  {
+    "revision": "78ec9275577226b0824205dd29eddcee",
+    "url": "/static/media/tanks2.78ec9275.png"
+  },
+  {
+    "revision": "fc0546663d3b5c30f8d417c6069b8c99",
+    "url": "/static/media/tanks3.fc054666.png"
+  },
+  {
+    "revision": "6cd5bedd49cc46b528544f5719940e51",
+    "url": "/static/media/tanks4.6cd5bedd.png"
+  },
+  {
+    "revision": "e650934462ee36f7a20003d7ed382193",
+    "url": "/static/media/spaceshooter.e6509344.jpg"
+  },
+  {
+    "revision": "bd45653b30e54b06d06259eb83e4e560",
+    "url": "/static/media/happier_cover.bd45653b.jpeg"
+  },
+  {
+    "revision": "24990080839f41b6b87c91662a29c384",
+    "url": "/static/media/child_cover.24990080.jpeg"
+  },
+  {
+    "revision": "e9c8891182e7775d8b37284c01d79cae",
+    "url": "/static/media/forever.e9c88911.jpeg"
+  },
+  {
+    "revision": "a53d3bd1540c525cee9d4fe77e5e8c58",
+    "url": "/static/media/alone.a53d3bd1.jpg"
+  },
+  {
+    "revision": "94ec433a4146e389173d6b38739940b5",
+    "url": "/static/media/hero.94ec433a.jpeg"
+  },
+  {
+    "revision": "bda28a4d4eec319edf1236f6b53c600e",
+    "url": "/index.html"
+  }
+];
